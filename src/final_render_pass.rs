433e42eb9ebda73d9d3682b3a059a96edfc8828d
LevKruglyak <lev.kruglyak2014@gmail.com>
2022-07-31 21:31:35 -0700

    resizable image view

---
+++ src/final_render_pass.rs
@@ -0,0 +1,340 @@
+use std::sync::Arc;
+
+use bytemuck::{Pod, Zeroable};
+use egui_winit_vulkano::Gui;
+use vulkano::{
+    buffer::{BufferUsage, CpuAccessibleBuffer, TypedBufferAccess, CpuBufferPool},
+    command_buffer::{
+        AutoCommandBufferBuilder, CommandBufferInheritanceInfo, CommandBufferUsage,
+        RenderPassBeginInfo, SubpassContents
+    },
+    descriptor_set::{PersistentDescriptorSet, WriteDescriptorSet},
+    device::{Device, Queue},
+    format::Format,
+    image::{ImageAccess, ImageViewAbstract},
+    impl_vertex,
+    pipeline::{
+        graphics::{
+            input_assembly::InputAssemblyState,
+            vertex_input::BuffersDefinition,
+            viewport::{Viewport, ViewportState},
+        },
+        GraphicsPipeline, Pipeline, PipelineBindPoint,
+    },
+    render_pass::{Framebuffer, FramebufferCreateInfo, RenderPass, Subpass},
+    sampler::{Filter, Sampler, SamplerAddressMode, SamplerCreateInfo},
+    sync::GpuFuture,
+};
+use vulkano_util::{
+    context::VulkanoContext,
+    renderer::{DeviceImageView, SwapchainImageView},
+};
+
+/// Simple read-only buffer type
+type Buffer<T> = Arc<CpuAccessibleBuffer<[T]>>;
+
+#[repr(C)]
+#[derive(Clone, Copy, Default, Pod, Zeroable)]
+struct QuadVertex {
+    position: [f32; 2],
+    tex_coords: [f32; 2],
+}
+impl_vertex!(QuadVertex, position, tex_coords);
+
+pub type ViewportTransform = vs::ty::Uniforms;
+
+pub struct FinalRenderPass {
+    device: Arc<Device>,
+    graphics_queue: Arc<Queue>,
+    render_pass: Arc<RenderPass>,
+    pipeline: Arc<GraphicsPipeline>,
+
+    // Viewport quad
+    vertex_buffer: Buffer<QuadVertex>,
+    index_buffer: Buffer<u32>,
+    uniform_buffer: CpuBufferPool<ViewportTransform>,
+}
+
+impl FinalRenderPass {
+    pub fn new(context: &VulkanoContext, format: Format) -> Self {
+        let render_pass = Self::create_render_pass(context.device(), format);
+        let pipeline = Self::create_pipeline(context.device(), render_pass.clone());
+        let (vertex_buffer, index_buffer) = Self::create_viewport_quad(context.device());
+        let uniform_buffer = CpuBufferPool::<ViewportTransform>::new(context.device(), BufferUsage::all());
+
+        Self {
+            device: context.device(),
+            graphics_queue: context.graphics_queue(),
+            render_pass,
+            pipeline,
+            vertex_buffer,
+            index_buffer,
+            uniform_buffer,
+        }
+    }
+
+    pub fn render<F>(
+        &self,
+        before_future: F,
+        target: SwapchainImageView,
+        gui: &mut Gui,
+        viewport_view: DeviceImageView,
+        viewport_bounds: Viewport,
+        viewport_transform: ViewportTransform,
+    ) -> Box<dyn GpuFuture>
+    where
+        F: GpuFuture + 'static,
+    {
+        // Get dimensions
+        let image_dimensions = target.image().dimensions();
+
+        // Create framebuffer (must be in same order as render pass description in `new`
+        let framebuffer = Framebuffer::new(
+            self.render_pass.clone(),
+            FramebufferCreateInfo {
+                attachments: vec![target],
+                ..Default::default()
+            },
+        )
+        .unwrap();
+
+        // Create primary command buffer builder
+        let mut primary_builder = AutoCommandBufferBuilder::primary(
+            self.device.clone(),
+            self.graphics_queue.family(),
+            CommandBufferUsage::OneTimeSubmit,
+        )
+        .unwrap();
+
+        // Begin render pass
+        primary_builder
+            .begin_render_pass(
+                RenderPassBeginInfo {
+                    clear_values: vec![Some([0.0, 0.0, 0.0, 1.0].into())],
+                    ..RenderPassBeginInfo::framebuffer(framebuffer)
+                },
+                SubpassContents::SecondaryCommandBuffers,
+            )
+            .unwrap();
+
+        // Create secondary command buffer from texture pipeline & send draw commands
+        let mut secondary_builder = AutoCommandBufferBuilder::secondary(
+            self.device.clone(),
+            self.graphics_queue.family(),
+            CommandBufferUsage::MultipleSubmit,
+            CommandBufferInheritanceInfo {
+                render_pass: Some(self.viewport_subpass().into()),
+                ..Default::default()
+            },
+        )
+        .unwrap();
+
+        let descriptor_set = self.create_descriptor_set(viewport_view, viewport_transform);
+        secondary_builder
+            .bind_pipeline_graphics(self.pipeline.clone())
+            .set_viewport(0, vec![viewport_bounds])
+            .bind_vertex_buffers(0, self.vertex_buffer.clone())
+            .bind_index_buffer(self.index_buffer.clone())
+            .bind_descriptor_sets(
+                PipelineBindPoint::Graphics,
+                self.pipeline.layout().clone(),
+                0,
+                descriptor_set,
+            )
+            .draw_indexed(self.index_buffer.len() as u32, 1, 0, 0, 0)
+            .unwrap();
+
+        // Render viewport
+        let cb = secondary_builder.build().unwrap();
+        primary_builder.execute_commands(cb).unwrap();
+
+        // Render gui
+        primary_builder
+            .next_subpass(SubpassContents::SecondaryCommandBuffers)
+            .unwrap();
+
+        let cb = gui.draw_on_subpass_image(image_dimensions.width_height());
+        primary_builder.execute_commands(cb).unwrap();
+
+        // End render pass
+        primary_builder.end_render_pass().unwrap();
+
+        // Build command buffer
+        let command_buffer = primary_builder.build().unwrap();
+
+        // Execute primary command buffer
+        let after_future = before_future
+            .then_execute(self.graphics_queue.clone(), command_buffer)
+            .unwrap();
+
+        after_future.boxed()
+    }
+
+    fn create_descriptor_set(
+        &self,
+        viewport_view: Arc<dyn ImageViewAbstract>,
+        viewport_transform: ViewportTransform,
+    ) -> Arc<PersistentDescriptorSet> {
+        let sampler = Sampler::new(
+            self.graphics_queue.device().clone(),
+            SamplerCreateInfo {
+                mag_filter: Filter::Nearest,
+                min_filter: Filter::Nearest,
+                address_mode: [SamplerAddressMode::ClampToBorder; 3],
+                ..Default::default()
+            },
+        )
+        .unwrap();
+
+        let uniform_buffer_subbuffer = self.uniform_buffer.next(viewport_transform).unwrap();
+
+        let layout = self.pipeline.layout().set_layouts().get(0).unwrap();
+        PersistentDescriptorSet::new(
+            layout.clone(),
+            [
+                WriteDescriptorSet::buffer(0, uniform_buffer_subbuffer),
+                WriteDescriptorSet::image_view_sampler(1, viewport_view.clone(), sampler)
+            ],
+        )
+        .unwrap()
+    }
+
+    fn create_render_pass(device: Arc<Device>, format: Format) -> Arc<RenderPass> { 
+        vulkano::ordered_passes_renderpass!(
+            device,
+            attachments: {
+                color: {
+                    load: Clear,
+                    store: Store,
+                    format: format,
+                    samples: 1,
+                }
+            },
+            passes: [
+                { color: [color], depth_stencil: {}, input: [] }, // Draw viewport result
+                { color: [color], depth_stencil: {}, input: [] }  // Gui render pass
+            ]
+        )
+        .expect("error creating final render pass")
+    }
+
+    fn create_pipeline(device: Arc<Device>, render_pass: Arc<RenderPass>) -> Arc<GraphicsPipeline> {
+        let vs = vs::load(device.clone()).expect("failed to create shader module");
+        let fs = fs::load(device.clone()).expect("failed to create shader module");
+
+        GraphicsPipeline::start()
+            .vertex_input_state(BuffersDefinition::new().vertex::<QuadVertex>())
+            .vertex_shader(vs.entry_point("main").unwrap(), ())
+            .input_assembly_state(InputAssemblyState::new())
+            .fragment_shader(fs.entry_point("main").unwrap(), ())
+            .viewport_state(ViewportState::viewport_dynamic_scissor_irrelevant())
+            .render_pass(Subpass::from(render_pass, 0).unwrap())
+            .build(device)
+            .expect("error creating pipeline")
+    }
+
+    fn create_viewport_quad(device: Arc<Device>) -> (Buffer<QuadVertex>, Buffer<u32>) {
+        let verticies = vec![
+            QuadVertex {
+                position: [-1.0, -1.0],
+                tex_coords: [0.0, 0.0],
+            },
+            QuadVertex {
+                position: [-1.0, 1.0],
+                tex_coords: [0.0, 1.0],
+            },
+            QuadVertex {
+                position: [1.0, 1.0],
+                tex_coords: [1.0, 1.0],
+            },
+            QuadVertex {
+                position: [1.0, -1.0],
+                tex_coords: [1.0, 0.0],
+            },
+        ];
+        let vertex_buffer = CpuAccessibleBuffer::<[QuadVertex]>::from_iter(
+            device.clone(),
+            BufferUsage::vertex_buffer(),
+            false,
+            verticies,
+        )
+        .expect("failed to create quad vertex buffer");
+
+        let indicies = vec![0, 2, 1, 0, 3, 2];
+        let index_buffer = CpuAccessibleBuffer::<[u32]>::from_iter(
+            device,
+            BufferUsage::index_buffer(),
+            false,
+            indicies,
+        )
+        .expect("failed to create quad index buffer");
+
+        (vertex_buffer, index_buffer)
+    }
+
+    pub fn viewport_subpass(&self) -> Subpass {
+        Subpass::from(self.render_pass.clone(), 0).expect("failed to create subpass")
+    }
+
+    pub fn gui_subpass(&self) -> Subpass {
+        Subpass::from(self.render_pass.clone(), 1).expect("failed to create subpass")
+    }
+}
+
+mod vs {
+    vulkano_shaders::shader! {
+        ty: "vertex",
+        src: "
+#version 450
+layout(location=0) in vec2 position;
+layout(location=1) in vec2 tex_coords;
+
+layout(location = 0) out vec2 f_tex_coords;
+
+layout(set = 0, binding = 0) uniform Uniforms {
+    vec2 offset;
+    float scale;
+    float aspect_ratio;
+} uniforms;
+
+void main() {
+    gl_Position =  vec4(uniforms.scale * (position * vec2(1.0, uniforms.aspect_ratio)) + uniforms.offset, 0.0, 1.0);
+    f_tex_coords = tex_coords;
+}
+        ",
+        types_meta: {
+            use bytemuck::{Pod, Zeroable};
+
+            #[derive(Clone, Copy, Zeroable, Pod)]
+        }
+    }
+}
+
+impl Default for vs::ty::Uniforms {
+    fn default() -> Self {
+        Self {
+            offset: [0.0, 0.0],
+            scale: 1.0,
+            aspect_ratio: 1.0,
+        }
+    }
+}
+
+mod fs {
+    vulkano_shaders::shader! {
+        ty: "fragment",
+        src: "
+#version 450
+layout(location = 0) in vec2 v_tex_coords;
+
+layout(location = 0) out vec4 f_color;
+
+layout(set = 0, binding = 1) uniform sampler2D tex;
+
+void main() {
+    f_color = texture(tex, v_tex_coords);
+    f_color = vec4(v_tex_coords, 0.0, 1.0);
+}
+"
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,203 @@
+use egui::Context;
+use egui_winit_vulkano::Gui;
+use lazy_static::lazy_static;
+use vulkano::{
+    device::{DeviceExtensions, Features},
+    format::Format,
+    image::ImageUsage,
+    instance::{InstanceCreateInfo, InstanceExtensions},
+    pipeline::graphics::viewport::Viewport,
+};
+use vulkano_util::{
+    context::{VulkanoConfig, VulkanoContext},
+    window::{VulkanoWindows, WindowDescriptor},
+};
+use winit::{
+    dpi::LogicalSize,
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+};
+
+use crate::final_render_pass::{FinalRenderPass, ViewportTransform};
+
+mod final_render_pass;
+
+lazy_static! {
+    static ref INSTANCE_EXTENSIONS: InstanceExtensions = InstanceExtensions {
+        ..vulkano_win::required_extensions()
+    };
+}
+
+const INSTANCE_LAYERS: Vec<String> = vec![];
+
+const DEVICE_EXTENSIONS: DeviceExtensions = DeviceExtensions {
+    khr_swapchain: true,
+    ..DeviceExtensions::none()
+};
+
+const FEATURES: Features = Features { ..Features::none() };
+
+// Required format by egui (otherwise egui colors look weird)
+const SWAPCHAIN_FORMAT: Format = Format::B8G8R8A8_SRGB;
+
+const WINDOW_SIZE: LogicalSize<f32> = LogicalSize::new(700.0, 500.0);
+const WINDOW_TITLE: &str = "Conway's Game of Life";
+
 fn main() {
-    println!("Hello, world!");
+    // Create vulkano context
+    let vulkano_context = VulkanoContext::new(VulkanoConfig {
+        instance_create_info: InstanceCreateInfo {
+            enabled_extensions: *INSTANCE_EXTENSIONS,
+            enabled_layers: INSTANCE_LAYERS,
+            ..Default::default()
+        },
+        device_features: FEATURES,
+        device_extensions: DEVICE_EXTENSIONS,
+        ..Default::default()
+    });
+
+    println!("Using device: {}", vulkano_context.device_name());
+
+    // Create window
+    let mut windows = VulkanoWindows::default();
+    let event_loop = EventLoop::new();
+    let main_window_id = windows.create_window(
+        &event_loop,
+        &vulkano_context,
+        &WindowDescriptor {
+            width: WINDOW_SIZE.width,
+            height: WINDOW_SIZE.height,
+            title: WINDOW_TITLE.to_string(),
+            ..WindowDescriptor::default()
+        },
+        |swapchain_create_info| {
+            swapchain_create_info.image_format = Some(SWAPCHAIN_FORMAT);
+        },
+    );
+    let window_renderer = windows.get_primary_renderer_mut().unwrap();
+
+    let final_render_pass = FinalRenderPass::new(&vulkano_context, SWAPCHAIN_FORMAT);
+    let viewport_view_id = 0;
+    window_renderer.add_additional_image_view(
+        viewport_view_id,
+        SWAPCHAIN_FORMAT,
+        ImageUsage { 
+            sampled: true,
+            storage: false,
+            color_attachment: true,
+            ..ImageUsage::none()
+        },
+    );
+
+    // Create gui context
+    let mut gui = Gui::new_with_subpass(
+        window_renderer.surface(),
+        vulkano_context.graphics_queue(),
+        final_render_pass.gui_subpass(),
+    );
+
+    let mut viewport_transform = ViewportTransform::default();
+
+    // Run the event loop to keep window open
+    event_loop.run(move |event, _, control_flow| {
+        let window_renderer = windows.get_primary_renderer_mut().unwrap();
+        
+        match event {
+            Event::WindowEvent { event, window_id } => {
+                if window_id == main_window_id {
+                    let pass_events_to_app = !gui.update(&event);
+                    if pass_events_to_app {
+                    }
+
+                    match event {
+                        WindowEvent::Resized(_) => {
+                            window_renderer.resize();
+                        }
+                        WindowEvent::ScaleFactorChanged { .. } => {
+                            window_renderer.resize();
+                        }
+                        WindowEvent::CloseRequested => {
+                            *control_flow = ControlFlow::Exit;
+                        }
+                        _ => (),
+                    }
+                }
+            }
+            Event::RedrawRequested(window_id) => {
+                if window_id == main_window_id {
+                    // Create immediate ui
+                    let context = gui.context();
+                    gui.immediate_ui(|_| {
+                        egui::SidePanel::left("left_panel")
+                            .min_width(200.0)
+                            .show(&context, |ui| {
+                            
+                            ui.vertical_centered(|ui| {
+                                ui.heading("Settings");
+                            });
+                            ui.separator();
+                            ui.horizontal(|ui| {
+                                ui.label("Offset X:");
+                                ui.add(egui::Slider::new(&mut viewport_transform.offset[0], -1.0..=1.0));
+                            });
+
+                            ui.horizontal(|ui| {
+                                ui.label("Offset Y:");
+                                ui.add(egui::Slider::new(&mut viewport_transform.offset[1], -1.0..=1.0));
+                            });
+
+                            ui.horizontal(|ui| {
+                                ui.label("Scale:");
+                                ui.add(egui::Slider::new(&mut viewport_transform.scale, 0.1..=50.0));
+                            });
+                        });
+                    });
+
+                    // Calculate viwport so as not to render behind egui components
+                    let viewport_bounds = calculate_viewport(
+                        &context,
+                        window_renderer.window().scale_factor() as f32,
+                    );
+
+                    // Update image aspect ratio
+                    viewport_transform.aspect_ratio = viewport_bounds.dimensions[0] / viewport_bounds.dimensions[1];
+
+                    let before_pipeline_future = window_renderer
+                        .acquire()
+                        .expect("failed to acquire window renderer future");
+                    let viewport_view = window_renderer.get_additional_image_view(viewport_view_id);
+
+                    // Render viewport
+
+                    // Render gui
+                    let after_gui_future = final_render_pass.render(
+                        before_pipeline_future,
+                        window_renderer.swapchain_image_view(),
+                        &mut gui,
+                        viewport_view,
+                        viewport_bounds,
+                        viewport_transform,
+                    );
+
+                    // Present to surface
+                    window_renderer.present(after_gui_future, true);
+                }
+            }
+            Event::MainEventsCleared => {
+                window_renderer.surface().window().request_redraw();
+            }
+            _ => (),
+        }
+    });
+}
+
+fn calculate_viewport(context: &Context, scale_factor: f32) -> Viewport {
+    let origin = context.available_rect().left_top();
+    let dimensions = context.available_rect().right_bottom() - origin;
+
+    Viewport {
+        origin: [origin.x * scale_factor, origin.y * scale_factor],
+        dimensions: [dimensions.x * scale_factor, dimensions.y * scale_factor],
+        depth_range: 0.0..1.0,
+    }
 }
